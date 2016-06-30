14e8c11f00e1871632e14d53df3e9c94b8bd4bbd
James <jsmilton05@gmail.com>
2016-06-30 10:50:08 +0100

    Initial Commit

---
+++ babylon-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  babylon-test
+//
+//  Created by James Milton on 30/06/2016.
+//  Copyright © 2016 james milton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
