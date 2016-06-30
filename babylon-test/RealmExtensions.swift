b96fbf1e45e65df1ca7002b08909863dddb4b390
James <jsmilton05@gmail.com>
2016-06-30 22:52:19 +0100

    stopped the awful, stuttering reloading animation

--- babylon-test/RealmExtensions.swift
+++ babylon-test/RealmExtensions.swift
@@ -18,14 +18,21 @@ extension Results {
                 tableView.reloadData()
                 break
             case .Update(_, let deletions, let insertions, let modifications):
-                tableView.beginUpdates()
-                tableView.insertRowsAtIndexPaths(insertions.map { NSIndexPath(forRow: $0, inSection: 0) },
-                    withRowAnimation: .Fade)
-                tableView.deleteRowsAtIndexPaths(deletions.map { NSIndexPath(forRow: $0, inSection: 0) },
-                    withRowAnimation: .Fade)
-                tableView.reloadRowsAtIndexPaths(modifications.map { NSIndexPath(forRow: $0, inSection: 0) },
-                    withRowAnimation: .Fade)
-                tableView.endUpdates()
+//                tableView.beginUpdates()
+//                tableView.insertRowsAtIndexPaths(insertions.map { NSIndexPath(forRow: $0, inSection: 0) },
+//                    withRowAnimation: .Fade)
+//                tableView.deleteRowsAtIndexPaths(deletions.map { NSIndexPath(forRow: $0, inSection: 0) },
+//                    withRowAnimation: .Fade)
+//                tableView.reloadRowsAtIndexPaths(modifications.map { NSIndexPath(forRow: $0, inSection: 0) },
+//                    withRowAnimation: .Fade)
+//                tableView.endUpdates()
+                
+                tableView.reloadData()
+                
+                // So Realm sends a modification update for every object, regardless of whether or not it has
+                // changed. Seems that reloading that many rows causes afwul performance in the tables reloading
+                // animation. That's why the above code is commented. Doing a full reload has excellent performance.
+                // Probably a way around it, but I haven't had a chance to find it yet.
                 break
             case .Error(let err):
                 fatalError("\(err)")
